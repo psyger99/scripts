0e532e9cdc39cbd3c1afc8778e06c3ee28fe0462
Rainer Alano <raineralano.rra@gmail.com>
2025-12-04 16:02:40 +0800

    Added which.ps1 in script-helper

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # SOURCED FUNCTIONS
 . C:\Users\rainer\Documents\PowerShell\script-helper\Get-FolderSize.ps1
 . C:\Users\rainer\Documents\PowerShell\script-helper\whereis.ps1
-
+. C:\Users\rainer\Documents\PowerShell\script-helper\which.ps1
 
 # SHELL INITIALIZATION
 # oh-my-posh init pwsh --config 'C:\Users\rainer\Documents\PowerShell\jblab2021.omp.json' | Invoke-Expression
